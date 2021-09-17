4c4d9bb146f0901ab126f0bfee99d630eb2601ce
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-09-17 17:37:05 +0530

    refactor - project structure

---
+++ src/moviedata.js
@@ -0,0 +1,57 @@
+const movies = [
+    {
+        id: 0,
+        name: 'Fight Club',
+        rating: 5,
+        genre: 'drama',
+    },
+    {
+        id: 1,
+        name: 'The Godfather',
+        rating: 5,
+        genre: 'drama',
+    },
+    {
+        id: 2,
+        name: 'The Shawshank Redemption',
+        rating: 5,
+        genre: 'drama',
+    },
+    {
+        id: 3,
+        name: 'The Dark Knight',
+        rating: 5,
+        genre: 'action',
+    },
+    {
+        id: 4,
+        name: 'The Lord of the Rings',
+        rating: 5,
+        genre: 'action',
+    },
+    {
+        id: 5,
+        name: 'Mission Impossible',
+        rating: 5,
+        genre: 'action',
+    },
+    {
+        id: 6,
+        name: 'Inception',
+        rating: 5,
+        genre: 'sci-fi',
+    },
+    {
+        id: 7,
+        name: 'Interstellar',
+        rating: 5,
+        genre: 'sci-fi',
+    },
+    {
+        id: 8,
+        name: 'Edge of Tomorrow',
+        rating: 5,
+        genre: 'sci-fi',
+    },
+
+]
