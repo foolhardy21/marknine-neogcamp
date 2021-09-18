5c9869ef19d959c34f6b9062a2f2bfb076fb0819
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-09-18 15:02:03 +0530

    feat - switch movies based on genres

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import React, { useState, useEffect } from 'react'
+import {movies, genres} from './moviedata'
 
 const App = () => {
+  const [genre, setGenre] = useState(0)
+  const [genreMovies, setGenreMovies] = useState([])
+  
+  useEffect(() => {
+    const newGenreMovies = movies.filter(movie => movie.genre === genre)
+    setGenreMovies([...newGenreMovies])
+  },[genre])
+
+  function changeGenre(id) {
+    setGenre(id)
+  }
   return (
     <div>
-      app component
+      <h2>movies are amazing</h2>
+      <p>Here are some of them...</p>
+      <div>{genres.map((genre, index) => <button key={index} onClick={() => changeGenre(index)}>{genre}</button>) }</div>
+      {
+        genreMovies.map(movie => <div key={movie.id}>{movie.name}</div>)
+      }
+
     </div>
   );
 }
--- src/moviedata.js
+++ src/moviedata.js
@@ -3,55 +3,59 @@ const movies = [
         id: 0,
         name: 'Fight Club',
         rating: 5,
-        genre: 'drama',
+        genre: 1,
     },
     {
         id: 1,
         name: 'The Godfather',
         rating: 5,
-        genre: 'drama',
+        genre: 1,
     },
     {
         id: 2,
         name: 'The Shawshank Redemption',
         rating: 5,
-        genre: 'drama',
+        genre: 1,
     },
     {
         id: 3,
         name: 'The Dark Knight',
         rating: 5,
-        genre: 'action',
+        genre: 0,
     },
     {
         id: 4,
         name: 'The Lord of the Rings',
         rating: 5,
-        genre: 'action',
+        genre: 0,
     },
     {
         id: 5,
         name: 'Mission Impossible',
         rating: 5,
-        genre: 'action',
+        genre: 0,
     },
     {
         id: 6,
         name: 'Inception',
         rating: 5,
-        genre: 'sci-fi',
+        genre: 2,
     },
     {
         id: 7,
         name: 'Interstellar',
         rating: 5,
-        genre: 'sci-fi',
+        genre: 2,
     },
     {
         id: 8,
         name: 'Edge of Tomorrow',
         rating: 5,
-        genre: 'sci-fi',
+        genre: 2,
     },
 
 ]
+
+const genres = ['action', 'drama', 'sci-fi']
+
+export {movies, genres}
