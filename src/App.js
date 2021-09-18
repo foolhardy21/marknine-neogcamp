35089495327e5342c64e06d71a0882a0b2f6de0b
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-09-18 15:17:36 +0530

    style - elements alignment and ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import {movies, genres} from './moviedata'
+import styles from './App.module.css'
 
 const App = () => {
   const [genre, setGenre] = useState(0)
   const [genreMovies, setGenreMovies] = useState([])
-  
+
   useEffect(() => {
     const newGenreMovies = movies.filter(movie => movie.genre === genre)
     setGenreMovies([...newGenreMovies])
@@ -17,9 +18,9 @@ const App = () => {
     <div>
       <h2>movies are amazing</h2>
       <p>Here are some of them...</p>
-      <div>{genres.map((genre, index) => <button key={index} onClick={() => changeGenre(index)}>{genre}</button>) }</div>
+      <div>{genres.map((genre, index) => <button key={index} onClick={() => changeGenre(index)} className={styles.genre_btn}>{genre}</button>) }</div>
       {
-        genreMovies.map(movie => <div key={movie.id}>{movie.name}</div>)
+        genreMovies.map(movie => <div key={movie.id} className={styles.movie}>{movie.name}</div>)
       }
 
     </div>
