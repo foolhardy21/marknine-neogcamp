8fe8b0ffd91f4d7f53315de1105bece01bf410f4
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-09-21 16:51:57 +0530

    style - movielist layout

--- src/App.js
+++ src/App.js
@@ -18,9 +18,15 @@ const App = () => {
     <div>
       <h2>movies are amazing</h2>
       <p>Here are some of them...</p>
-      <div>{genres.map((genre, index) => <button key={index} onClick={() => changeGenre(index)} className={styles.genre_btn}>{genre}</button>) }</div>
+      <div className={styles.genrelist}>{genres.map((genre, index) => <button key={index} onClick={() => changeGenre(index)} className={styles.genre_btn}>{genre}</button>) }
+      </div>
       {
-        genreMovies.map(movie => <div key={movie.id} className={styles.movie}>{`${movie.name} ${movie.rating}/5`}</div>)
+        genreMovies.map(movie => 
+        <div key={movie.id} className={styles.movie}>
+          <h4>{movie.name}</h4>
+          <p className={styles.movie_desc}>{movie.desc}</p>
+          <span className={styles.span}>{movie.rating}/10</span>
+        </div>)
       }
 
     </div>
